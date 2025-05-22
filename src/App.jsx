590f22904f02673a3843bfa840420923798f964e
Mohammad Shoeb Ansari <mshoebansari477@gmail.com>
2025-05-22 13:33:40 +0530

    comment diya

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <h1>Faique</h1>
+    <h1>Faique  || Fardin</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
